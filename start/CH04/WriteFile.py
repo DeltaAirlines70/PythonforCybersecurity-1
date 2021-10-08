f798c83fcc3557b4cdc3d254378dc2f3b654dde3
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-08 16:53:09 -0700

    Updatii

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 # Sample script that writes to a file
 # By 
+# Intro to cryptography
+
+# How cryptography *can work* - ROT13
+  test_file = open("testfile.txt", "w")
+  test_file.write("Hello World")
+  test_file.write("My name is Ed\n")
+  test_file.write("I like rubber ducks\n")
+
+  test_file.close()
