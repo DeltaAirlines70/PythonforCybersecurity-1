60560110a228201fb95bd70d138ae9a30a1c31ae
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-08 16:12:19 -0700

    Changingstuff

--- start/CH05/Base64.py
+++ start/CH05/Base64.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # Script that "encrypts"/"decrypts" text using base64 encoding
 # By 
-import base64
+import Base64
 
 def encode_data(plain_text):
     plain_text = plain_text.encode()
-    cipher_text = base64.b64encode(plain_text)
+    cipher_text = Base64.b64encode(plain_text)
     cipher_text = cipher_text.decode()
     return cipher_text
 def decode_data(cipher_text):
-    plain_text = base64.b64decode(cipher_text)
+    plain_text = Base64.b64decode(cipher_text)
     return plain_text
 
     method = input("Do you wish to Encode or Decode (e/d)? ")
