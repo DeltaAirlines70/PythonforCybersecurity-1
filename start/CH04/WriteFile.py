67b64a066ffaed2a7a1fabf0446dc12b2918fb91
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-08 16:56:00 -0700

    Reading

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -4,9 +4,9 @@
 # Intro to cryptography
 
 # How cryptography *can work* - ROT13
-  test_file = open("testfile.txt", "w")
-  test_file.write("Hello World")
-  test_file.write("My name is Ed\n")
-  test_file.write("I like rubber ducks\n")
+test_file = open("testfile.txt", "w")
+test_file.write("Hello World")
+test_file.write("My name is Ed\n")
+test_file.write("I like rubber ducks\n")
 
-  test_file.close()
+test_file.close()
