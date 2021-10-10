d5c04ad7d6dbb8afeaa574126d33288baaaa754f
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-09 23:52:03 -0700

    ChangestoGitjiub

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -5,8 +5,11 @@
 
 # How cryptography *can work* - ROT13
 test_file = open("testfile.txt", "w")
+ 
 test_file.write("Hello World")
 test_file.write("My name is Ed\n")
 test_file.write("I like rubber ducks\n")
-
-test_file.close()
+test_file.write("We'd like to make you an offer")
+test_file.write("How we hire at Google")
+#test_file.write("College Stress")
+#test_file.close()
