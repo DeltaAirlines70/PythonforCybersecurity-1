06e2c26ce63deb424d469b6eed110c9852e5cf66
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-10 23:04:49 -0700

    changedtoelse

--- start/CH05/CryptExample.py
+++ start/CH05/CryptExample.py
@@ -33,7 +33,7 @@ elif method == "d":
     cipher_text = input("Message to Encrypt or Decrypt")
     plain_text = decrpyt(cipher_text, encKey)
     print(plain_text)
-elif method != "c" and method != "d" and method != "e":
+else:
     print("sorry, please enter c, d or r")
 #if method != "c" and method != "d" and  method != "e":
   # d
