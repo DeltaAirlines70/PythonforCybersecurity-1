727c9004950ab2da455537ce0781fe6f6859abdd
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-12 23:49:45 -0700

    UpdatedtheWriteFile

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -2,17 +2,21 @@
 # Sample script that writes to a file
 # By 
 # Intro to cryptography
-
 # How cryptography *can work* - ROT13
-test_file = open("testfile.txt", "w")
- 
-test_file.write("Hello World")
-test_file.write("My name is Ed\n")
-test_file.write("I like rubber ducks\n")
-test_file.write("We'd like to make you an offer")
-test_file.write("How we hire at Google")
-test_file.write("squares on the man")
- 
- 
-#test_file.write("College Stress")
-#test_file.close()
+test_file = open("hackme.txt", "w") 
+print("What is your name?")
+name = input()
+test_file.write("Your name is", name)
+print("What is your favorite color?")
+color = input()
+test_file.write("Your favorite color is ", color)
+print("What was the name of your first pet you had?")
+petsname = input()
+test_file.write("The name of your first pet you had is ", petsname)
+print("What is your mother's maiden name")
+maidename = input()
+test_file.write("My mother's maiden name is ", maidename)
+print("What elementary school did you attend")
+elementaryschool = input()
+test_file.write("The elementary school I attened was", elementaryschool)
+test_file.close()
