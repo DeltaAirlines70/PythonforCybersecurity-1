85501a1fb1c8404ea4e93d9f417491d32a98c31c
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-13 00:10:48 -0700

    MovedThingsToTheBottom

--- start/CH04/WriteFile.py
+++ start/CH04/WriteFile.py
@@ -6,17 +6,24 @@
 test_file = open("hackme.txt", "w") 
 print("What is your name?")
 name = input()
-test_file.write("Your name is", name)
+
 print("What is your favorite color?")
 color = input()
-test_file.write("Your favorite color is ", color)
+
 print("What was the name of your first pet you had?")
 petsname = input()
-test_file.write("The name of your first pet you had is ", petsname)
+
 print("What is your mother's maiden name")
 maidename = input()
-test_file.write("My mother's maiden name is ", maidename)
+
 print("What elementary school did you attend")
 elementaryschool = input()
+
+test_file.write("Your name is", name)
+test_file.write("Your favorite color is ", color)
+test_file.write("The name of the first pet I had is ", petsname)
 test_file.write("The elementary school I attened was", elementaryschool)
+test_file.write("My mother's maiden name is ", maidename)
+
+
 test_file.close()
