5eb54905ef58661f1108ab7cde059054a68b076a
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-14 22:49:58 -0700

    Addedtheport

--- start/CH04/pinger6.py
+++ start/CH04/pinger6.py
@@ -4,3 +4,15 @@
 # By 
 import nmap
 target_address = "192.168.0.10"
+
+port_start = 1
+port_end = 100
+
+scanner = nmap.PortScanner()
+
+print("Scanning {0}".format(target_address))
+
+for port in range(port_start, port_end + 1):
+    result = scanner.scan(target_address, str(port))
+    port_status = result['scan'][target_address]['tcp'][port]['state']
+    print("\tPort: {0} is {1}".format(port, port_status))
