f535964ecf7c98577486702cd74a58d42df1dab3
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-18 10:46:04 -0700

    ceatedstuff

--- start/CH06/CreateHash2.py
+++ start/CH06/CreateHash2.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # Script that hashes a password with provided salt
 # By 
+import crypt
+
+plain_pass = input("What is the password? ")
+salt = input("What is the salt? ")
+
+print("MD5       : {0}".format(crypt.crypt(plain_pass,"$1$" + salt)))
+print("Blowfish  : {0}".format(crypt.crypt(plain_pass,"$2$" + salt)))
+print("eksblofish: {0}".format(crypt.crypt(plain_pass,"$2a$" + salt)))
+print("SHA-256   : {0}".format(crypt.crypt(plain_pass, "$5$" + salt)))
+print("SHA-512   : {0}".format(crypt.crypt(plain_pass, "$6$" + salt)))
