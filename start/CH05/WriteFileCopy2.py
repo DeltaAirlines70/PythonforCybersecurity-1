7b79a12447fc853d75f99ec4e238d315dd7f2ca0
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-20 22:01:33 -0700

    DeletedLines

--- start/CH05/WriteFileCopy2.py
+++ start/CH05/WriteFileCopy2.py
@@ -1,12 +1,8 @@
 import os
-
 script_path = os.path.realpath(__file__)
 script_folder = os.path.split(script_path)
-
 test_file = open(script_folder[0] + "/testfile.txt", "w")
-
 test_file.write("Hello world\n")
 test_file.write("\tanother line\n")
 test_file.write("Last line\n")
-
 test_file.close()
