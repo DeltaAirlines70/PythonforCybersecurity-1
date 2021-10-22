e6ffda90afb5ce5d9a15ad5cebd7be26a73e8839
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-22 14:23:49 -0700

    Deletedans

--- start/CH05/ReadFile.py
+++ start/CH05/ReadFile.py
@@ -1,5 +1,5 @@
 import os
-script_path = os.realpat(__file__)
+script_path = os.realpath(__file__)
 script_folder = os.path.split(script_path)
 ip_file = open(script_folder[0]+ "/testfile.txt", "r")
 ip_addresses = ip_file.read()
