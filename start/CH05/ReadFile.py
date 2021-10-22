fd7c0cd79e2074620a4ce8754a533aa3acf06f97
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-22 14:29:10 -0700

    Editedaline

--- start/CH05/ReadFile.py
+++ start/CH05/ReadFile.py
@@ -1,8 +1,9 @@
 import os
-script_path = os.realpath(__file__)
+script_path = os.path.realpath(__file__)
 script_folder = os.path.split(script_path)
-ip_file = open(script_folder[0]+ "/ips.txt", "r")
+ip_file = open(script_folder[0]+ "/testfile.txt", "r")
 ip_addresses = ip_file.read()
 print(ip_addresses)
+print(ip_file.read())
 
 ip_file.close()
