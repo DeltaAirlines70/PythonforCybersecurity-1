0a555258d86222b1c8736d060bc4594b0fa9a384
DeltaAirlines70 <Banerjee.Caleb@student.greenriver.edu>
2021-10-25 21:15:54 -0700

    enteredstuff

--- start/CH06/passHasher.py
+++ start/CH06/passHasher.py
@@ -6,3 +6,6 @@
 # Password: Password01
 # Salt: G.DTW7g9s5U7KYf5
 # SHA-512 result: $6$G.DTW7g9s5U7KYf5$xTXAbS1Q30hfd10VDbkSh5adZMxbqRUMOyNyKopfFpMvD.Vf/CcoEBn/TUYcfJ1jAaEiJPBf/PoCLFq7U7Q7p.
+o = open("shadow.txt", "r")
+for line in o:
+# print the user from the shadow.txt and then print out the password from the top1000.txt
